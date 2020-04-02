cf8e0450ee269d2df3c1bf50ed94a15b80d263f7
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2020-04-02 10:41:14 +0200

    feat(lights-controller) Add a configuration file.

--- lights/lights-controller/src/main.rs
+++ lights/lights-controller/src/main.rs
@@ -1,7 +1,12 @@
+use confy;
+use directories::ProjectDirs;
 use human_panic::setup_panic;
+use serde::{Deserialize, Serialize};
 use std::{
+    default::Default,
     io::prelude::*,
     net::{SocketAddr, TcpStream},
+    path::PathBuf,
 };
 use structopt::{clap::arg_enum, StructOpt};
 
@@ -37,16 +42,18 @@ arg_enum! {
 #[structopt(name = "lights-controller")]
 struct Options {
     /// Address of the Controllino; see `lights.ino` to see the port;
-    /// e.g. `192.168.1.42:23`.
+    /// e.g. `192.168.1.42:23`. This option overwrites the value read
+    /// from the configuration file.
     #[structopt(short, long)]
-    address: SocketAddr,
+    address: Option<SocketAddr>,
 
     /// Light to control.
     #[structopt(
         short,
         long,
         possible_values = &Subject::variants(),
-        case_insensitive = true
+        case_insensitive = true,
+        default_value = "LivingRoom",
     )]
     subject: Subject,
 
@@ -56,19 +63,68 @@ struct Options {
         long,
         possible_values = &Action::variants(),
         case_insensitive = true,
-        default_value = "pulse"
+        default_value = "Pulse",
     )]
     action: Action,
+
+    /// Print the configuration path and exit.
+    #[structopt(short = "c", long)]
+    print_config_path: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Configuration {
+    address: SocketAddr,
+}
+
+impl Default for Configuration {
+    fn default() -> Self {
+        Self {
+            address: "127.0.0.1:23".parse().unwrap(),
+        }
+    }
+}
+
+fn configuration_path() -> Result<PathBuf, &'static str> {
+    let name = "lights-controller";
+    let project = ProjectDirs::from("rs", "", name)
+        .ok_or_else(|| "Failed to find the configuration project directory.")?;
+
+    let directory = project
+        .config_dir()
+        .to_str()
+        .ok_or_else(|| "Failed to find the configuration directory.")?;
+
+    let path: PathBuf = [directory, &format!("{}.toml", name)].iter().collect();
+
+    Ok(path)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     setup_panic!();
 
+    let configuration_path = configuration_path()?;
+    let configuration: Configuration = confy::load_path(&configuration_path)?;
+
     let options = Options::from_args();
-    let mut stream = TcpStream::connect(options.address)?;
+
+    if options.print_config_path {
+        println!(
+            "{}",
+            configuration_path
+                .into_os_string()
+                .into_string()
+                .unwrap_or_else(|e| format!("{:?}", e))
+        );
+
+        return Ok(());
+    }
+
+    let mut stream = TcpStream::connect(options.address.unwrap_or(configuration.address))?;
 
     println!("Sending a {:?} to {:?}…", options.action, options.subject);
 
+    // The real piece of code.
     stream.write(&[options.subject as u8, b'\t', options.action as u8])?;
 
     Ok(())
