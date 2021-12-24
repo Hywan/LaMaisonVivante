b2366db56991cefa283c5348d61bcaf4d13e4442
Ivan Enderlin <ivan@mnt.io>
2021-12-24 11:58:49 +0100

    feat(ui) Dirty code to add a secondary property to a meter thing.

--- hub/ui/static/javascript/main.js
+++ hub/ui/static/javascript/main.js
@@ -146,7 +146,8 @@ window.customElements.define(
                 thing.setAttribute('id', 'meter-thing-' + thing_index);
                 thing_index += 1;
 
-                const thing_value_element = template_content.querySelector('.thing--meter-value');
+                const thing_primary_value_element = template_content.querySelector('.thing--meter-primary-value');
+                const thing_secondary_value_element = template_content.querySelector('.thing--meter-secondary-value');
                 const thing_meter_circle_element = template_content.querySelector('.thing--meter-meter > .meter--blend > circle');
 
                 const shadow_root = this.attachShadow({mode: 'open'})
@@ -155,16 +156,16 @@ window.customElements.define(
                 const self = this;
                 const base = self.getAttribute('data-base').replace(/\/+$/, '');
                 const base_origin = new URL(base).origin;
-                const property_name = self.getAttribute('data-property');
+                const primary_property_name = self.getAttribute('data-property');
 
-                const property_response = await http_get(base);
-                const property_json_response = await property_response.json();
+                const primary_property_response = await http_get(base);
+                const primary_property_json_response = await primary_property_response.json();
 
-                const property_description = property_json_response.properties[property_name];
-                const property_unit = property_description.unit;
-                const property_link = property_description.links[0].href;
+                const primary_property_description = primary_property_json_response.properties[primary_property_name];
+                const primary_property_unit = primary_property_description.unit;
+                const primary_property_link = primary_property_description.links[0].href;
 
-                async function update_value() {
+                async function update_value(thing_value_element, property_name, property_link, property_unit, do_update_thing_meter_circle_element) {
                     const response = await http_get(base_origin + property_link);
                     const json_response = await response.json();
                     const value = json_response[property_name];
@@ -173,15 +174,48 @@ window.customElements.define(
                     switch (property_unit) {
                     case 'percent':
                         formatted_value += '%';
+                        break;
+
+                    case 'watt':
+                        formatted_value += 'W';
+                        break;
                     }
 
                     thing_value_element.innerHTML = formatted_value;
-                    thing_meter_circle_element.style.strokeDasharray = value + ' 100';
 
-                    window.setTimeout(update_value, 1000 * 30 /* in 30 secs */, false);
+                    if (do_update_thing_meter_circle_element) {
+                        thing_meter_circle_element.style.strokeDasharray = value + ' 100';
+                    }
+
+                    window.setTimeout(
+                        () => {
+                            update_value(
+                                thing_value_element,
+                                property_name,
+                                property_link,
+                                property_unit,
+                                do_update_thing_meter_circle_element
+                            );
+                        },
+                        1000 * 30 /* in 30 secs */,
+                        false
+                    );
                 }
 
-                update_value();
+                update_value(thing_primary_value_element, primary_property_name, primary_property_link, primary_property_unit, true);
+
+                if (self.hasAttribute('data-secondary-property')) {
+                    const secondary_property_name = self.getAttribute('data-secondary-property');
+
+                    const secondary_property_response = await http_get(base);
+                    const secondary_property_json_response = await secondary_property_response.json();
+
+                    const secondary_property_description = secondary_property_json_response.properties[secondary_property_name];
+                    const secondary_property_unit = secondary_property_description.unit;
+                    const secondary_property_link = secondary_property_description.links[0].href;
+
+                    update_value(thing_secondary_value_element, secondary_property_name, secondary_property_link, secondary_property_unit, false);
+                }
             }
         };
     }
