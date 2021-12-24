fb862fcd74facdbb571576bd8b19820802af852d
Ivan Enderlin <ivan@mnt.io>
2021-12-24 17:35:53 +0100

    feat(ui) Refactor code.

--- hub/ui/static/javascript/main.js
+++ hub/ui/static/javascript/main.js
@@ -26,6 +26,42 @@ function http_post(url, body) {
     })
 }
 
+async function read_property(base, property_name) {
+    const base_origin = new URL(base).origin;
+    const property_response = await http_get(base);
+    const property_json_response = await property_response.json();
+
+    const property_description = property_json_response.properties[property_name];
+    const property_unit = property_description.unit;
+    const property_link = property_description.links[0].href;
+
+    return {
+        link: property_link,
+        unit: property_unit,
+        value_reader: async function () {
+            const response = await http_get(base_origin + property_link);
+            const json_response = await response.json();
+            const value = json_response[property_name];
+            let formatted_value = value;
+
+            switch (property_unit) {
+            case 'percent':
+                formatted_value += '%';
+                break;
+
+            case 'watt':
+                formatted_value += 'W';
+                break;
+            }
+
+            return {
+                value,
+                formatted_value,
+            };
+        },
+    };
+}
+
 window.customElements.define(
     'my-nav',
     class extends HTMLElement {
@@ -153,33 +189,8 @@ window.customElements.define(
                 const shadow_root = this.attachShadow({mode: 'open'})
                       .appendChild(template_content);
 
-                const self = this;
-                const base = self.getAttribute('data-base').replace(/\/+$/, '');
-                const base_origin = new URL(base).origin;
-                const primary_property_name = self.getAttribute('data-property');
-
-                const primary_property_response = await http_get(base);
-                const primary_property_json_response = await primary_property_response.json();
-
-                const primary_property_description = primary_property_json_response.properties[primary_property_name];
-                const primary_property_unit = primary_property_description.unit;
-                const primary_property_link = primary_property_description.links[0].href;
-
-                async function update_value(thing_value_element, property_name, property_link, property_unit, do_update_thing_meter_circle_element) {
-                    const response = await http_get(base_origin + property_link);
-                    const json_response = await response.json();
-                    const value = json_response[property_name];
-                    let formatted_value = value;
-
-                    switch (property_unit) {
-                    case 'percent':
-                        formatted_value += '%';
-                        break;
-
-                    case 'watt':
-                        formatted_value += 'W';
-                        break;
-                    }
+                async function update_value(thing_value_element, property_value_reader, property_link, property_unit, do_update_thing_meter_circle_element) {
+                    const {value, formatted_value} = await property_value_reader();
 
                     thing_value_element.innerHTML = formatted_value;
 
@@ -191,7 +202,7 @@ window.customElements.define(
                         () => {
                             update_value(
                                 thing_value_element,
-                                property_name,
+                                property_value_reader,
                                 property_link,
                                 property_unit,
                                 do_update_thing_meter_circle_element
@@ -202,19 +213,28 @@ window.customElements.define(
                     );
                 }
 
-                update_value(thing_primary_value_element, primary_property_name, primary_property_link, primary_property_unit, true);
+                const self = this;
+                const base = self.getAttribute('data-base').replace(/\/+$/, '');
+                const primary_property = await read_property(base, self.getAttribute('data-property'));
+
+                update_value(
+                    thing_primary_value_element,
+                    primary_property.value_reader,
+                    primary_property.link,
+                    primary_property.unit,
+                    true,
+                );
 
                 if (self.hasAttribute('data-secondary-property')) {
-                    const secondary_property_name = self.getAttribute('data-secondary-property');
+                    const secondary_property = await read_property(base, self.getAttribute('data-secondary-property'));
 
-                    const secondary_property_response = await http_get(base);
-                    const secondary_property_json_response = await secondary_property_response.json();
-
-                    const secondary_property_description = secondary_property_json_response.properties[secondary_property_name];
-                    const secondary_property_unit = secondary_property_description.unit;
-                    const secondary_property_link = secondary_property_description.links[0].href;
-
-                    update_value(thing_secondary_value_element, secondary_property_name, secondary_property_link, secondary_property_unit, false);
+                    update_value(
+                        thing_secondary_value_element,
+                        secondary_property.value_reader,
+                        secondary_property.link,
+                        secondary_property.unit,
+                        false,
+                    );
                 }
             }
         };
