07461101310de2dfe427387ef33d2e6d4e6c7470
Ivan Enderlin <ivan@mnt.io>
2024-04-18 09:12:43 +0200

    feat(weather): Move to One Call API 3.0.

--- weather/src/reader.rs
+++ weather/src/reader.rs
@@ -3,7 +3,7 @@ use reqwest::Result;
 
 pub fn read(openweathermap_api_key: &str) -> Result<State> {
     reqwest::blocking::get(format!(
-        "https://api.openweathermap.org/data/2.5/onecall?appid={openweathermap_api_key}&lat={latitude}&lon={longitude}&units=metric&lang={language}&exclude=daily,minutely",
+        "https://api.openweathermap.org/data/3.0/onecall?appid={openweathermap_api_key}&lat={latitude}&lon={longitude}&units=metric&lang={language}&exclude=daily,minutely",
         openweathermap_api_key = openweathermap_api_key,
         latitude = HOME_LATITUDE,
         longitude = HOME_LONGITUDE,
